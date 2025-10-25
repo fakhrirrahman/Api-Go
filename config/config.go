6c496338cf66d868a40c41db615a7c4cd966be8e
fakhrirrahman <fakhrirrahman7@gmail.com>
2025-10-25 16:10:16 +0700

    Add configuration management with godotenv and update dependencies
    - Introduced config package to load environment variables from .env file
    - Updated go.mod and go.sum to include godotenv dependency
    - Added .env to .gitignore to prevent it from being tracked

---
+++ config/config.go
@@ -0,0 +1,34 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	AppName    string
+	AppPort    string
+	DBHost     string
+	DBPort     string
+	DBUser     string
+	DBPassword string
+	DBName     string
+}
+
+func LoadConfig() *Config {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("No .env file found, using system environment variables")
+	}
+	return &Config{
+		AppName:    os.Getenv("APP_NAME"),
+		AppPort:    os.Getenv("APP_PORT"),
+		DBHost:     os.Getenv("DB_HOST"),
+		DBPort:     os.Getenv("DB_PORT"),
+		DBUser:     os.Getenv("DB_USER"),
+		DBPassword: os.Getenv("DB_PASSWORD"),
+		DBName:     os.Getenv("DB_NAME"),
+	}
+}
